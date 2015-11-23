4ee25fc0e7081a207d297e900a9e911b526afaa3
Jiwon Park <jiwonpark81@gmail.com>
2015-11-23 14:12:41 +0900

    add table row dynamically

---
+++ pigfarm/public/js/task.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+	$('#fertilization').append(
+		'<tr>'+
+		'	<td>1</td>'+
+		'	<td>Y 39-12</td>'+
+		'	<td>이유모돈</td>'+
+		'	<td>2산</td>'+
+		'	<td>5일</td>'+
+		'	<td>정액1차</td>'+
+		'	<td>정액2차</td>'+
+		'	<td>정액3차</td>'+
+		'	<td>라유</td>'+
+		'	<td>완료</td>'+
+		'	<td class="text-right">'+
+		'		<div class="btn-group">'+
+		'			<a href="#" data-toggle="dropdown" class="dropdown-toggle">'+
+		'				<i class="fa fa-pencil"></i>'+
+		'			</a>'+
+		'			<ul class="dropdown-menu pull-right">'+
+		'				<li><a href="#">Action</a></li>'+
+		'				<li><a href="#">Another action</a></li>'+
+		'				<li><a href="#">Something else here</a></li>'+
+		'				<li class="divider"></li>'+
+		'				<li><a href="#">Separated link</a></li>'+
+		'			</ul>'+
+		'		</div>'+
+		'	</td>'+
+		'</tr>'
+	);
+});
