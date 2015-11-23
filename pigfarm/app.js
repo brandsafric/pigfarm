47ff9685bff68b7cc8ae58043c0a3b1afc8f7652
Jiwon Park <jiwonpark81@gmail.com>
2015-11-23 09:32:13 +0900

    "work" -> "task"

--- pigfarm/app.js
+++ pigfarm/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var work = require('./routes/work');
+var task = require('./routes/task');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/work', work);
+app.use('/task', task);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- pigfarm/routes/task.js
+++ pigfarm/routes/task.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('work', { title: 'Express' });
+  res.render('task', { title: 'Express' });
 });
 
 module.exports = router;
