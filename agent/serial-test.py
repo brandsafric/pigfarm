6f6493a97421ecc56b56d30d4664b529f6701d5a
Jiwon Park <jiwonpark81@gmail.com>
2016-01-02 20:09:36 +0900

    make agent more robust

--- agent/serial-test.py
+++ agent/serial-test.py
@@ -12,8 +12,11 @@ try:
 		print response
 		t = response.split(" ")[1].split("C")[0]
 		print t
-		r = requests.post("http://ec2-52-25-245-158.us-west-2.compute.amazonaws.com:3000/monitor/temperature", data={'temperature': t})
-		print(r.status_code, r.reason)
-		print(r.text[:300] + '...')
+		try:
+			r = requests.post("http://ec2-52-25-245-158.us-west-2.compute.amazonaws.com:3000/monitor/temperature", data={'temperature': t})
+			print(r.status_code, r.reason)
+			print(r.text[:300] + '...')
+		except:
+			print("server not responding")
 except KeyboardInterrupt:
 	ser.close()
