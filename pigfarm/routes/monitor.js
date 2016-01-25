6063111dc3293e282cefc68bd268c030187c3415
Jiwon Park <jiwonpark81@gmail.com>
2016-01-25 21:37:27 +0900

    log last ping time and update agent status

--- pigfarm/routes/monitor.js
+++ pigfarm/routes/monitor.js
@@ -9,21 +9,33 @@ router.get('/temperature/:count', function(req, res) {
 	var db = req.db;
 	var collection = db.get('temperature');
 	var count = req.params.count;
-	console.log(count);
+//	console.log(count);
 	collection.find( {}, { sort : {_id:-1}, limit : count }, function(e,docs){
-		console.log(docs);
+//		console.log(docs);
 		res.json(docs);
 	});
 });
 
 router.post('/temperature', function(req, res) {
 	var db = req.db;
+
+	var agentNum = '1001001';
+
 	var collection = db.get('temperature');
-	console.log(req.body);
+	req.body.num = agentNum;
+	req.body.dt = new Date();
+//	console.log(req.body);
 	collection.insert(req.body, function(err, result) {
-		console.log(result);
+		console.log('temperature insert ' + (result ? 'success' : 'fail!'));
 		res.send((err == null) ? { msg : '', id : result._id } : { msg : err });
 	});
+
+	var collection2 = db.get('agent');
+	var agent = { num : agentNum, lastPing : new Date() };
+//	console.log(agent);
+	collection2.update( { num : agentNum }, agent, { upsert : true }, function(err, result) {
+		console.log('agent(' + agentNum + ') status update ' + (result ? 'success' : 'fail!'));
+	})
 });
 
 module.exports = router;
